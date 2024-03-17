caab33a35e509e53a3b4b6655929d7a1d3d09c25
Exx1piee <mishapron2008@gmail.com>
2024-03-17 14:20:58 +0200

    next pushing

--- contacts.js
+++ contacts.js
@@ -16,3 +16,28 @@ return JSON.parse(data);
 };
 
 
+export const getContactById = async (contactId) => {
+const contacts = await listContacts();
+const result = contacts.find((item)=> item.id ===contactId );
+return result || null ;
+};
+
+
+export const removeContactById = async (contactId) => {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateContacts(contacts);
+  
+    return result;
+  };
+
+
+
+
+  export const addContact = async (name , email , phone) =>{
+
+};
