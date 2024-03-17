2861dadc328154f7fd0b421f9af0b8573f81c845
Exx1piee <mishapron2008@gmail.com>
2024-03-17 21:33:02 +0200

    patching

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { program } from "commander";
-import * as contactsService from "./contacts.js";
+import { addContact, getContactById, listContacts, deleteContactById } from './contacts.js';
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -14,21 +14,24 @@ program
   async function invokeAction({ action, id, name, email, phone }) {
     switch (action) {
       case "list":
-        const allContacts = await contactsService.listContacts();
-        return console.log(allContacts);
+        const allContacts = await listContacts();
+        console.table(allContacts);
+        break;
   
       case "get":
-        const oneContact = await contactsService.getContactById(id);
-        return console.log(oneContact);
+        const oneContact = await getContactById(id);
+        console.log(oneContact);
+        break;
   
       case "add":
-        const newContact = await contactsService.addContact(name, email, phone);
-        return console.log(newContact);
+        const newContact = await addContact(name, email, phone);
+        console.log(newContact);
+        break;
   
       case "remove":
-        const deleteContact = await contactsService.removeContactById(id);
-        return console.log(deleteContact);
-  
+        const deleteContact = await deleteContactById(id);
+        console.log(deleteContact);
+  break;
       default:
         console.warn("\x1B[31m Unknown action type!");
     }
