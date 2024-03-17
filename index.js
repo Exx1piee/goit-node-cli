d1a7555c3e28c3e72e86fdf3c3bb7c3a9e1b22c2
Exx1piee <mishapron2008@gmail.com>
2024-03-17 21:09:38 +0200

    aa

--- index.js
+++ index.js
@@ -7,28 +7,31 @@ program
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
 
-program.parse();
+  program.parse();
 
-const options = program.opts();
-
-async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-     const allContacts = await contactsService.listContacts();
-    return console.log (allContacts);
-
-    case "get":
-const oneContact = await contactsService.getContactById(id);
-return console.log(oneContact);
-    case "add":
-    const newContact = await contactsService.addContact(name , email , phone);
-return console.log(newContact);
-    case "remove":
-const removeContact = await contactsService.removeContactById(id)
-return console.log(removeContact);
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+  const options = program.opts();
+  
+  async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case "list":
+        const allContacts = await contactsService.listContacts();
+        return console.log(allContacts);
+  
+      case "get":
+        const oneContact = await contactsService.getContactById(id);
+        return console.log(oneContact);
+  
+      case "add":
+        const newContact = await contactsService.addContact(name, email, phone);
+        return console.log(newContact);
+  
+      case "remove":
+        const deleteContact = await contactsService.removeContactById(id);
+        return console.log(deleteContact);
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
   }
-}
-
-invokeAction(options);
+  
+  invokeAction(options);
